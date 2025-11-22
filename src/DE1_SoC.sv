fdbd6d5afe8d169948e5642e9045280ae723c54c
rhitries <rgarnier@uw.edu>
2025-11-22 11:52:19 -0800

    started the game board!

--- src/DE1_SoC.sv
+++ src/DE1_SoC.sv
@@ -19,19 +19,17 @@ module DE1_SoC (HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, KEY, LEDR, SW,
 	logic [9:0] x;
 	logic [8:0] y;
 	logic [7:0] r, g, b;
-
-	pixel_counter pc (.clk(CLOCK_50), .reset(reset), .x, .y);
 	
 	video_driver #(.WIDTH(640), .HEIGHT(480))
 		v1 (.CLOCK_50, .reset, .x, .y, .r, .g, .b,
 			 .VGA_R, .VGA_G, .VGA_B, .VGA_BLANK_N,
 			 .VGA_CLK, .VGA_HS, .VGA_SYNC_N, .VGA_VS);
 	
-	always_ff @(posedge CLOCK_50) begin
-		r <= SW[7:0];
-		g <= x[7:0];
-		b <= y[7:0];
-	end
+	// always_ff @(posedge CLOCK_50) begin
+	// 	r <= SW[7:0];
+	// 	g <= x[7:0];
+	// 	b <= y[7:0];
+	// end
 	
 	assign HEX0 = '1;
 	assign HEX1 = '1;
@@ -46,17 +44,23 @@ module DE1_SoC (HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, KEY, LEDR, SW,
 		// each address is a block
 		// each address holds info about what type of block it should be
 	// then there is a secondary rom that stores the pixels for each type of block
-		
 
 	logic [4:0] block_x; // 0 through 31
 	logic [4:0] block_y; // 0 through 23
 	logic [4:0] local_x; // 0 through 19, for each pixel in the block
 	logic [4:0] local_y; // 0 through 19, for each pixel in the block
+	logic [3:0] block_type; // the type of block to draw
 
 	assign block_x  = x / 20;        
 	assign block_y  = y / 20;        
 	assign local_x = x % 20;        
-	assign local_y = y % 20;        
+	assign local_y = y % 20;  
+
+	initial_board_rom mem_board (.address(block_y * 32 + block_x), .clock(CLOCK_50), .q(block_type));
+
+	// type_rom_address = block_type * 400 + (local_y * 20 + local_x)
+	// output of type_rom is pixel color {r, g, b}
+	type_rom mem_type (.address(block_type * 400 + (local_y * 20 + local_x)), .clock(CLOCK_50), .q({r, g, b})); 
 	
 	
 endmodule  // DE1_SoC
